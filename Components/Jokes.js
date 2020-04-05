834e43c363f7da60391a286bf2ab5d7776f0eff9
mmakutonin <michael.makutonin@gmail.com>
2020-04-05 19:53:02 -0400

    Revert "Updated Joke Button"
    This reverts commit 16984d68345b0628313a43540af238f02a2c6a71.

--- Components/Jokes.js
+++ Components/Jokes.js
@@ -1,9 +1,9 @@
 import React, { Jokes, useState }from 'react';
 import axios from 'axios';
 
-const JokeButton = () => {
+const Jokes = () => {
 
-    const[Jokes, setJoke] = useState("")
+    const[joke, setJoke] = useState("")
     Jokes(() => {
         const fetchData = async () => {
             const result = await axios ("https://us-central1-dadsofunny.cloudfunctions.net/DadJokes/random/type/general")
